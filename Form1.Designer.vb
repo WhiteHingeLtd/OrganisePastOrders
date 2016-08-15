8bf52867f69bcada671d7793156a74ba44a3298b
whcmcaloon <colin@whitehinge.com>
2016-08-15 13:19:30 +0100

    Files
    New program!

---
+++ Form1.Designer.vb
@@ -0,0 +1,235 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.GetFromOrdersBtn = New System.Windows.Forms.Button()
+        Me.GetFromPastBtn = New System.Windows.Forms.Button()
+        Me.SendFilesBtn = New System.Windows.Forms.Button()
+        Me.GatheredGrid = New System.Windows.Forms.DataGridView()
+        Me.DestinationCB = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.InfoLabel = New System.Windows.Forms.Label()
+        Me.LoadingProgress = New System.Windows.Forms.ProgressBar()
+        Me.SendOldBtn = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PastSubText = New System.Windows.Forms.TextBox()
+        Me.GetFromOtherBtn = New System.Windows.Forms.Button()
+        Me.OrderNumCol = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.SelectedCol = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.OrderCreationDateCol = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.OrderStateCol = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.GatheredGrid, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GetFromOrdersBtn
+        '
+        Me.GetFromOrdersBtn.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GetFromOrdersBtn.Location = New System.Drawing.Point(198, 12)
+        Me.GetFromOrdersBtn.Name = "GetFromOrdersBtn"
+        Me.GetFromOrdersBtn.Size = New System.Drawing.Size(119, 23)
+        Me.GetFromOrdersBtn.TabIndex = 0
+        Me.GetFromOrdersBtn.Text = "Gather from Orders"
+        Me.GetFromOrdersBtn.UseVisualStyleBackColor = True
+        '
+        'GetFromPastBtn
+        '
+        Me.GetFromPastBtn.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GetFromPastBtn.Location = New System.Drawing.Point(323, 12)
+        Me.GetFromPastBtn.Name = "GetFromPastBtn"
+        Me.GetFromPastBtn.Size = New System.Drawing.Size(119, 23)
+        Me.GetFromPastBtn.TabIndex = 1
+        Me.GetFromPastBtn.Text = "Gather from Past"
+        Me.GetFromPastBtn.UseVisualStyleBackColor = True
+        '
+        'SendFilesBtn
+        '
+        Me.SendFilesBtn.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SendFilesBtn.Location = New System.Drawing.Point(355, 558)
+        Me.SendFilesBtn.Name = "SendFilesBtn"
+        Me.SendFilesBtn.Size = New System.Drawing.Size(87, 23)
+        Me.SendFilesBtn.TabIndex = 2
+        Me.SendFilesBtn.Text = "Send Selected"
+        Me.SendFilesBtn.UseVisualStyleBackColor = True
+        '
+        'GatheredGrid
+        '
+        Me.GatheredGrid.AllowUserToAddRows = False
+        Me.GatheredGrid.AllowUserToDeleteRows = False
+        Me.GatheredGrid.AllowUserToResizeRows = False
+        Me.GatheredGrid.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GatheredGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.GatheredGrid.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.OrderNumCol, Me.SelectedCol, Me.OrderCreationDateCol, Me.OrderStateCol})
+        Me.GatheredGrid.Location = New System.Drawing.Point(12, 71)
+        Me.GatheredGrid.Name = "GatheredGrid"
+        Me.GatheredGrid.RowHeadersVisible = False
+        Me.GatheredGrid.Size = New System.Drawing.Size(430, 481)
+        Me.GatheredGrid.TabIndex = 3
+        '
+        'DestinationCB
+        '
+        Me.DestinationCB.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DestinationCB.FormattingEnabled = True
+        Me.DestinationCB.Items.AddRange(New Object() {"Date-based folder", "Orders folder"})
+        Me.DestinationCB.Location = New System.Drawing.Point(129, 560)
+        Me.DestinationCB.Name = "DestinationCB"
+        Me.DestinationCB.Size = New System.Drawing.Size(140, 21)
+        Me.DestinationCB.TabIndex = 4
+        Me.DestinationCB.Text = "---Please Select---"
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 563)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(111, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Send selected files to:"
+        '
+        'InfoLabel
+        '
+        Me.InfoLabel.AutoSize = True
+        Me.InfoLabel.Location = New System.Drawing.Point(12, 17)
+        Me.InfoLabel.Name = "InfoLabel"
+        Me.InfoLabel.Size = New System.Drawing.Size(128, 13)
+        Me.InfoLabel.TabIndex = 6
+        Me.InfoLabel.Text = "Gather files to get started."
+        '
+        'LoadingProgress
+        '
+        Me.LoadingProgress.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LoadingProgress.Location = New System.Drawing.Point(15, 587)
+        Me.LoadingProgress.Name = "LoadingProgress"
+        Me.LoadingProgress.Size = New System.Drawing.Size(427, 29)
+        Me.LoadingProgress.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.LoadingProgress.TabIndex = 7
+        '
+        'SendOldBtn
+        '
+        Me.SendOldBtn.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SendOldBtn.Location = New System.Drawing.Point(275, 558)
+        Me.SendOldBtn.Name = "SendOldBtn"
+        Me.SendOldBtn.Size = New System.Drawing.Size(74, 23)
+        Me.SendOldBtn.TabIndex = 8
+        Me.SendOldBtn.Text = "Send Old"
+        Me.SendOldBtn.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 44)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(184, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Load from: T:\AppData\Orders\Past\"
+        '
+        'PastSubText
+        '
+        Me.PastSubText.Location = New System.Drawing.Point(198, 41)
+        Me.PastSubText.Name = "PastSubText"
+        Me.PastSubText.Size = New System.Drawing.Size(119, 20)
+        Me.PastSubText.TabIndex = 10
+        '
+        'GetFromOtherBtn
+        '
+        Me.GetFromOtherBtn.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GetFromOtherBtn.Enabled = False
+        Me.GetFromOtherBtn.Location = New System.Drawing.Point(323, 41)
+        Me.GetFromOtherBtn.Name = "GetFromOtherBtn"
+        Me.GetFromOtherBtn.Size = New System.Drawing.Size(119, 23)
+        Me.GetFromOtherBtn.TabIndex = 11
+        Me.GetFromOtherBtn.Text = "Gather from Other"
+        Me.GetFromOtherBtn.UseVisualStyleBackColor = True
+        '
+        'OrderNumCol
+        '
+        Me.OrderNumCol.HeaderText = "Order Number"
+        Me.OrderNumCol.Name = "OrderNumCol"
+        Me.OrderNumCol.ReadOnly = True
+        '
+        'SelectedCol
+        '
+        Me.SelectedCol.HeaderText = "Selected"
+        Me.SelectedCol.Name = "SelectedCol"
+        '
+        'OrderCreationDateCol
+        '
+        DataGridViewCellStyle1.Format = "d"
+        DataGridViewCellStyle1.NullValue = Nothing
+        Me.OrderCreationDateCol.DefaultCellStyle = DataGridViewCellStyle1
+        Me.OrderCreationDateCol.HeaderText = "Created on"
+        Me.OrderCreationDateCol.Name = "OrderCreationDateCol"
+        Me.OrderCreationDateCol.ReadOnly = True
+        '
+        'OrderStateCol
+        '
+        Me.OrderStateCol.HeaderText = "Order State"
+        Me.OrderStateCol.Name = "OrderStateCol"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(454, 628)
+        Me.Controls.Add(Me.GetFromOtherBtn)
+        Me.Controls.Add(Me.PastSubText)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.SendOldBtn)
+        Me.Controls.Add(Me.LoadingProgress)
+        Me.Controls.Add(Me.InfoLabel)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DestinationCB)
+        Me.Controls.Add(Me.GatheredGrid)
+        Me.Controls.Add(Me.SendFilesBtn)
+        Me.Controls.Add(Me.GetFromPastBtn)
+        Me.Controls.Add(Me.GetFromOrdersBtn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.GatheredGrid, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GetFromOrdersBtn As Button
+    Friend WithEvents GetFromPastBtn As Button
+    Friend WithEvents SendFilesBtn As Button
+    Friend WithEvents GatheredGrid As DataGridView
+    Friend WithEvents DestinationCB As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents InfoLabel As Label
+    Friend WithEvents LoadingProgress As ProgressBar
+    Friend WithEvents SendOldBtn As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PastSubText As TextBox
+    Friend WithEvents GetFromOtherBtn As Button
+    Friend WithEvents OrderNumCol As DataGridViewTextBoxColumn
+    Friend WithEvents SelectedCol As DataGridViewCheckBoxColumn
+    Friend WithEvents OrderCreationDateCol As DataGridViewTextBoxColumn
+    Friend WithEvents OrderStateCol As DataGridViewTextBoxColumn
+End Class
---
+++ Form1.vb
@@ -0,0 +1,270 @@
+﻿Imports WHLClasses
+Imports Linnworks.Orders.ExtendedOrder
+
+Public Class Form1
+    Dim FilesGathered As Boolean = False
+    Dim DestinationPath As String = ""
+    Dim Loader As New GenericDataController
+    Dim LoadedFromOrders As Boolean = False
+    Dim LoadedFromPast As Boolean = False
+    Dim LoadedFromOther As Boolean = False 'Will we ever do this? Maybe. If we decide to get particular past folders...
+    Dim LoadedFolderLocation As String = "" 'This will contain that folder's location and load from there. Let's ensure maximum accessibility
+
+    'Dim listOfOrders As Linnworks.Orders.ExtendedOrder 'NOPE. there's 22k files in that past folder. No. Don't hold all that info. Are you NUTS.
+    Private Sub GatherItems(FilePath As String)
+        GatheredGrid.Rows.Clear() 'We gotta clear this out on running a second time.
+
+        Dim LoadOrderStatus As Boolean = False
+        If MsgBox("Would you like to load order states? This will take longer as it has to load EACH file individually to get this information", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+            LoadOrderStatus = True
+        End If
+
+        MyBase.Enabled = False 'PREVENT BUTTONS BEING PRESSED
+
+        InfoLabel.Text = "Getting file list." 'Let the user know we haven't frozen
+        Application.DoEvents()
+
+        'Dim FileList As List(Of String) = My.Computer.FileSystem.GetFiles(FilePath).ToList 'Get the list
+
+        Dim FileList As List(Of String) = My.Computer.FileSystem.GetFiles(FilePath, FileIO.SearchOption.SearchTopLevelOnly, {"*.ordex"}).ToList 'Get the count
+        LoadingProgress.Maximum = FileList.Count 'Loading bar - equivelant to the amount of files
+        LoadingProgress.Minimum = 0 'Starting at 0
+
+        InfoLabel.Text = FileList.Count.ToString + " found. Loading." 'Let the user know we haven't frozen
+        Application.DoEvents()
+
+        Dim DateOfFile As Date
+        For Each file In FileList
+            DateOfFile = My.Computer.FileSystem.GetFileInfo(file).CreationTime '.ToString("yyyy/MM/dd")
+
+            Dim newRow As New DataGridViewRow 'Make a row
+            newRow.CreateCells(GatheredGrid) 'Create cells
+            newRow.Cells(0).Value = file.Replace(FilePath, "") 'Add the file name to the first column
+            newRow.Cells(1).Value = False 'This will be fuggin awkward but we can do this
+            newRow.Cells(2).Value = DateOfFile 'Add the state date to the second column
+
+            If LoadOrderStatus Then
+                Try 'Awww shit this is gonna suck... We can't just use the orddef because it just checks orders. Past isn't gonna work like that.
+                    Dim theOrdex As Linnworks.Orders.ExtendedOrder = Loader.LoadOrdex(file, False)
+                    newRow.Cells(3).Value = theOrdex.Status.ToString 'Add the file state to the second column
+                Catch ex As Exception
+                    'When we get loading stuff ready, we need to have our reports added to the table somewhere for when things go wrong...
+                    'Maybe add the name to a list of failed records, then add them to a fail list table in a different window...
+                End Try
+            End If
+
+            newRow.Tag = file
+            GatheredGrid.Rows.Add(newRow) 'Add the row
+
+            LoadingProgress.Increment(1)
+        Next
+
+        LoadingProgress.Value = 0
+
+        'InfoLabel.Text =  'We have our files and values ready. Tell the user what we're loading
+        'Application.DoEvents()
+
+        'For Each file As String In fileDictionary.Keys 'For each file in the list
+        '    If file.EndsWith(".ordex") Then 'Check they're ordex files
+
+
+        '        Dim newRow As New DataGridViewRow 'Make a row
+        '        newRow.CreateCells(GatheredGrid) 'Create cells
+        '        newRow.Cells(0).Value = file.Replace(FilePath, "") 'Add the file name to the first column
+        '        newRow.Cells(1).Value = False 'This will be fuggin awkward but we can do this
+        '        newRow.Cells(2).Value = fileDictionary.Item(file) 'Add the state date to the second column
+
+        '        Try 'Awww shit this is gonna suck... We can't just use the orddef because it just checks orders. Past isn't gonna work like that.
+        '            Dim theOrdex As Linnworks.Orders.ExtendedOrder = Loader.LoadOrdex(file, False)
+        '            newRow.Cells(3).Value = theOrdex.Status.ToString 'Add the file state to the second column
+        '        Catch ex As Exception
+        '            'When we get loading stuff ready, we need to have our reports added to the table somewhere for when things go wrong...
+        '            'Maybe add the name to a list of failed records, then add them to a fail list table in a different window...
+        '        End Try
+
+        '        newRow.Tag = file
+
+
+        '        LoadingProgress.Increment(1)
+
+        '        GatheredGrid.Rows.Add(newRow) 'Add the row
+        '    End If
+        'Next
+        'LoadingProgress.Value = 0 'We're done. Stop looking like we're loading.
+        Application.DoEvents()
+
+        MyBase.Enabled = True 'LET BUTTONS BE PRESSED
+
+        FilesGathered = True 'We've gathered our files. Let our "move" button know it can run.
+    End Sub
+
+    Private Sub GetFromOrdersBtn_Click(sender As Object, e As EventArgs) Handles GetFromOrdersBtn.Click
+        GatherItems("T:\AppData\Orders\")
+        LoadedFromOrders = True
+        LoadedFromPast = False
+        LoadedFromOther = False
+        InfoLabel.Text = "Files from 'Orders' folder."
+    End Sub
+
+    Private Sub GetFromPastBtn_Click(sender As Object, e As EventArgs) Handles GetFromPastBtn.Click
+        GatherItems("T:\AppData\Orders\Past\")
+        LoadedFromOrders = False
+        LoadedFromPast = True
+        LoadedFromOther = False
+        InfoLabel.Text = "Files from 'Past' folder."
+    End Sub
+
+    Private Sub GetFromOtherBtn_Click(sender As Object, e As EventArgs) Handles GetFromOtherBtn.Click
+        If PastSubText.Text = "" Then
+            MsgBox("Please enter a folder name into the location text box.")
+        Else
+            Dim locString As String = PastSubText.Text
+            If Not locString.EndsWith("\") Then 'Make sure we format this right
+                locString += "\"
+            End If
+            LoadedFolderLocation = "T:\AppData\Orders\Past\" + locString 'Get items from subfolders
+
+            Try
+                GatherItems(LoadedFolderLocation)
+
+                LoadedFromOrders = False
+                LoadedFromPast = False
+                LoadedFromOther = True
+                InfoLabel.Text = "Files from 'Past' folder."
+
+            Catch ex As Exception
+                MsgBox("Could not find " + LoadedFolderLocation + ". Please make sure the text box has been formatted correctly and that the folder exists.")
+            End Try
+        End If
+    End Sub
+
+    Private Sub SendFilesBtn_Click(sender As Object, e As EventArgs) Handles SendFilesBtn.Click
+        If DestSelected Then
+            If LoadedFromOrders Then
+                'AAAHHHH YOU ETERNAL BROKEN PENCIL! YOU'RE POINTLESS!
+                'I will be back, with an army of coffee. Mark my words, code. You will be given a purpose.
+
+                MsgBox("Moving from orders is now no longer possible. Too much will go wrong.")
+
+                'If DestinationPath = "T:\AppData\Orders\" Then
+                '    'Stop wasting time :/
+                '    MsgBox("This will send " + fileDictionary.Count.ToString + " orders from the orders folder to... the orders folder!? Cancelling.")
+
+                'Else
+                '    'This isn't gonna be too complex. Just send them to DestinationPath + fileDictionary.Item(<KEY VALUE>).Replace("\", "-") + "\"
+                '    If MsgBox("This will send " + fileDictionary.Count.ToString + " orders from the orders folder to folders based on their creation date. Are you sure?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                '        For Each file In fileDictionary
+                '            Dim DestPath As String = DestinationPath + file.Value.Replace("/", "-") + "\" + file.Key.Replace("T:\AppData\Orders\", "") 'This will become T:\AppData\Orders\Past\yyyy-MM-dd\1234567.ordex
+                '            My.Computer.FileSystem.MoveFile(file.Key, DestPath) 'Send T:\AppData\Orders\1234567.ordex to T:\AppData\Orders\Past\yyyy-MM-dd\1234567.ordex
+                '        Next
+                '    End If
+                'End If
+            Else 'Loaded from Past
+                If DestinationPath = "T:\AppData\Orders\" Then
+
+                    LoadingProgress.Maximum = GatheredGrid.Rows.Count 'Loading bar - equivelant to the amount of files
+                    LoadingProgress.Minimum = 0 'Starting at 0
+
+                    Dim newFileDictionary As New Dictionary(Of String, String) 'Get our selected rows
+                    For Each row As DataGridViewRow In GatheredGrid.Rows 'For each row
+                        If row.Cells(1).Value = True Then 'Check it's ticked
+                            Dim newKVP As KeyValuePair(Of String, String) = row.Tag 'Get the tag, a dictionary entry - A key and value
+                            newFileDictionary.Add(newKVP.Key, newKVP.Value) 'Add it to the new dictionary
+                            LoadingProgress.Increment(1) 'Increase our loading bar
+                        End If
+                    Next
+                    LoadingProgress.Value = 0 'Set loading bar back.
+
+                    LoadingProgress.Maximum = newFileDictionary.Count 'Loading bar - equivelant to the amount of files... redone
+
+                    If MsgBox("This will send " + newFileDictionary.Count.ToString + " orders from the past folder to the orders folder. Are you sure?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                        For Each file In newFileDictionary
+                            'This is why we needed the new file dictionary. Without it, we'd have issues.
+                            Dim DestPath As String = DestinationPath + file.Key.Replace("T:\AppData\Orders\Past\", "") 'This will become    T:\AppData\Orders\   +   1234567.ordex
+                            My.Computer.FileSystem.MoveFile(file.Key, DestPath) 'Send T:\AppData\Orders\Past\1234567.ordex to T:\AppData\Orders\1234567.ordex
+                            LoadingProgress.Increment(1) 'Increase our loading bar
+                        Next
+                    End If
+                    LoadingProgress.Value = 0 'Set loading bar back.
+                Else
+                    'If MsgBox("This will send " + fileDictionary.Count.ToString + " orders from the past folder to folders based on their creation date. Are you sure?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                    '    For Each file In fileDictionary
+
+                    '        'Move 'file' to 'DestinationPath + file.Value.Replace("\", "-") + "\"'
+
+
+                    '    Next
+                    'End If
+
+                    MsgBox("Not happening. It'll screw up Oversold Order Viewer and Order Downloader which have to see old orders.")
+                End If
+
+            End If
+
+            InfoLabel.Text = "Files sent."
+        Else
+            MsgBox("Select a destination folder from the drop down menu.")
+        End If
+
+    End Sub
+
+    Private Sub SendOldBtn_Click(sender As Object, e As EventArgs) Handles SendOldBtn.Click
+        If DestSelected Then
+
+            MyBase.Enabled = False 'PREVENT BUTTONS BEING PRESSED
+            LoadingProgress.Maximum = GatheredGrid.Rows.Count 'Loading bar - equivelant to the amount of files
+            LoadingProgress.Minimum = 0 'Starting at 0
+            LoadingProgress.Value = 0
+
+            If MsgBox("This will send orders in this table from a week ago, regardless of whether selected or not, into the target folder. Are you sure you want to do this?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                If MsgBox("ALL FILES" + vbNewLine + "FROM OVER A WEEK AGO" + vbNewLine + "ARE GOING TO MOVE." + vbNewLine + vbNewLine + "YOU'RE CERTAIN?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+
+                    InfoLabel.Text = "Moving files. This may take some time." 'Let the user know we haven't frozen
+                    MyBase.Enabled = False
+
+                    Application.DoEvents()
+
+                    Dim DateOfFile As Date = Today.AddDays(-7) 'Get a week in the past
+                    For Each Row As DataGridViewRow In GatheredGrid.Rows
+                        If Row.Cells(2).Value < DateOfFile Then 'Check if files are a week old
+                            Dim theFile As String = Row.Tag
+                            If DestinationPath = "T:\AppData\Orders\" Then
+                                Dim DestPath As String = DestinationPath + theFile.Replace("T:\AppData\Orders\", "").Replace("Past\", "")
+                                My.Computer.FileSystem.MoveFile(theFile, DestPath)
+                            Else 'OOOH, we have to seperate by date :D
+                                Dim tagDate As DateTime = Row.Cells(2).Value
+                                Dim DestPath As String = DestinationPath + tagDate.ToString("yyyy-MM-dd") + "\" + theFile.Replace("T:\AppData\Orders\", "").Replace("Past\", "") 'This will become T:\AppData\Orders\Past\yyyy-MM-dd\1234567.ordex
+                                My.Computer.FileSystem.MoveFile(theFile, DestPath) 'Send T:\AppData\Orders\1234567.ordex to T:\AppData\Orders\Past\yyyy-MM-dd\1234567.ordex
+
+                            End If
+                        End If
+
+                        LoadingProgress.Increment(1)
+                    Next
+                    LoadingProgress.Value = 0
+                    InfoLabel.Text = "Files moved."
+                    GatheredGrid.Rows.Clear()
+                    MyBase.Enabled = True
+
+                    Application.DoEvents()
+
+                End If
+            End If
+        Else
+            MsgBox("Select a destination folder from the drop down menu.")
+        End If
+    End Sub
+
+    Private DestSelected As Boolean = False
+    Private Sub DestinationCB_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DestinationCB.SelectedValueChanged
+        If DestinationCB.Text = "Date-based folder" Then
+            DestinationPath = "T:\AppData\Orders\Past\"
+            DestSelected = True
+        ElseIf DestinationCB.Text = "Orders folder" Then
+            DestinationPath = "T:\AppData\Orders\"
+            DestSelected = True
+        End If
+    End Sub
+
+
+End Class
